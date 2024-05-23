a1a64e2eaccc29a6be01e3ece6f61adfce39b0e3
Thigas05 <thi.purcino.militao@gmail.com>
2024-05-22 22:09:25 -0300

    quarto commit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-let nomeDoHeroi = (Thigas)
+let nomeDoHeroi = ("Thigas")
 let xp = 500
 
 if (xp >= 10001)
-    console.log ("esse Herói é do nível Radiante")
+    console.log (" é do nível Radiante") 
+else if (xp >= 9001 && xp <= 10000)
+    console.log (" é do nível Imortal")
+else if (xp >= 8001 && xp <= 9000) 
+    console.log (" é do nível Ascendente")
+else if (xp >= 7001 && xp <= 8000) 
+    console.log (" é do nível Platina")
+else if (xp >= 5001 && xp <= 7000) 
+    console.log (" é do nível Ouro")
+else if (xp >= 2001 && xp <= 5000) 
+    console.log (" é do nível Prata")
+else if (xp >= 1001 && xp <= 2000) 
+    console.log (" é do nível Bronze")
+else console.log (" é do nível Ferro")
 
-console.log ("O héroi" + nomeDoHeroi + xp)
+console.log ("O Herói " + nomeDoHeroi + xp)
